cefba573dcfec8e843e351e0cfca0afbdb130e3e
benoit moret <benoit.moret@gmail.com>
2025-05-19 15:51:56 +0200

    auth completed with sessiondetails

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::SessionsController < ApplicationController
+  skip_before_action :authenticate_request, only: [:create]
+  def create
+    user = User.find_by(email: params[:email])
+    p params
+    puts "user: #{user}"
+    if user&.authenticate(params[:password])
+      token = Auth::JsonWebToken.encode(user_id: user.id)
+      render json: { token: token }, status: :ok
+    else
+      render json: { error: 'Email ou mot de passe invalide' }, status: :unauthorized
+    end
+  end
+
+  def show
+    render json: { user_email: @current_user.email }, status: :ok
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_request #toutes les actions sont bloq
+
+  attr_reader :current_user
+
+  private
+  # Cette méthode utilise le module Auth::AuthenticateRequest pour vérifier la validité du token et définir l'utilisateur courant. Si le token est invalide ou absent, une réponse 401 est renvoyée.
+  def authenticate_request
+    @current_user = Auth::AuthenticateRequest.new(request.headers).call
+    render json: { error: 'Non autorisé' }, status: :unauthorized unless @current_user
+  end
 end
