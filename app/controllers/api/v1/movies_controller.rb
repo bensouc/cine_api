2e0add489c0196dfa8d6f605b00d627c4939eb0c
benoit moret <benoit.moret@gmail.com>
2025-05-21 12:23:10 +0200

    upgrade Error message

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -2,12 +2,21 @@ class Api::V1::MoviesController < ApplicationController
   def index
     @movies = Movie.limit(580).includes(:director, :actors)
   end
+
   def create
     @movie = Movie.new(movie_params)
     if @movie.save
       render json: @movie, status: :created
     else
-      render json: @movie.errors, status: :unprocessable_entity
+      render json: {
+        "errors": [
+          {
+            "status": '422',
+            "title": 'Movie creation failed',
+            "detail": "#{@movie.errors.full_messages.join(', ')}"
+          }
+        ]
+      }, status: :unprocessable_entity
     end
   end
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::API
   # Cette méthode utilise le module Auth::AuthenticateRequest pour vérifier la validité du token et définir l'utilisateur courant. Si le token est invalide ou absent, une réponse 401 est renvoyée.
   def authenticate_request
     @current_user = Auth::AuthenticateRequest.new(request.headers).call
-    render json: { error: 'Non autorisé' }, status: :unauthorized unless @current_user
+    render json: { error: 'Unauthorized' }, status: :unauthorized unless @current_user
   end
 end
